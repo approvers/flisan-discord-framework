4f3e7e460e63532b4df9e1e01383d641e1c29076
loxygenK <loxygen.k@gmail.com>
2020-10-11 18:01:49 +0900

    †修正† Unused Importがあった

--- src/main/kotlin/dev/approvers/jubilant/client/Client.kt
+++ src/main/kotlin/dev/approvers/jubilant/client/Client.kt
@@ -11,7 +11,6 @@ import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.events.ReadyEvent
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
-import java.lang.NullPointerException
 import kotlin.system.exitProcess
 
 /**
