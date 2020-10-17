92a754b66b3dc8a1296b95bc8e97544e568358e7
loxygenK <loxygen.k@gmail.com>
2020-10-17 12:29:43 +0900

    †追加† Sendableを生やしてそれを使うことにした

--- src/main/kotlin/dev/approvers/jubilant/commands/ResultMessageFormatter.kt
+++ src/main/kotlin/dev/approvers/jubilant/commands/ResultMessageFormatter.kt
@@ -1,5 +1,6 @@
 package dev.approvers.jubilant.commands
 
+import dev.approvers.jubilant.type.sendable.Sendable
 import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 
@@ -11,7 +12,7 @@ interface ResultMessageFormatter {
      * @param subCommand サブコマンド名。
      * @param event 解析しているイベント。
      */
-    fun onCommandSucceed(command: String, subCommand: String, event: MessageReceivedEvent): Message
+    fun onCommandSucceed(command: String, subCommand: String, event: MessageReceivedEvent): Sendable
 
     /**
      * コマンドの実行に失敗した時のメッセージを整形する。
@@ -19,7 +20,7 @@ interface ResultMessageFormatter {
      * @param subCommand サブコマンド名。
      * @param event 解析しているイベント。
      */
-    fun onCommandFailed(command: String, subCommand: String, event: MessageReceivedEvent): Message
+    fun onCommandFailed(command: String, subCommand: String, event: MessageReceivedEvent): Sendable
 
     /**
      * コマンドの実行中に例外が発生した時のメッセージを整形する。
@@ -28,7 +29,7 @@ interface ResultMessageFormatter {
      * @param event 解析しているイベント。
      * @param exception 発生した例外。
      */
-    fun onExceptionThrown(command: String, subCommand: String, event: MessageReceivedEvent, exception: Exception): Message
+    fun onExceptionThrown(command: String, subCommand: String, event: MessageReceivedEvent, exception: Exception): Sendable
 
     /**
      * コマンドに不正な引数が渡された時のメッセージを整形する。
@@ -36,14 +37,14 @@ interface ResultMessageFormatter {
      * @param subCommand サブコマンド名。
      * @param event 解析しているイベント。
      */
-    fun onInvalidArgumentsPassed(command: String, subCommand: String, event: MessageReceivedEvent): Message
+    fun onInvalidArgumentsPassed(command: String, subCommand: String, event: MessageReceivedEvent): Sendable
 
     /**
      * 登録されていないコマンドが実行されようとした時のメッセージを整形する。
      * @param command 実行されようとしたコマンド名。
      * @param event 解析しているイベント。
      */
-    fun onUnknownCommandPassed(command: String, event: MessageReceivedEvent): Message
+    fun onUnknownCommandPassed(command: String, event: MessageReceivedEvent): Sendable
 
     /**
      * コマンドは存在するが、存在しないサブコマンドが実行されようとした時のメッセージを整形する。
@@ -51,6 +52,6 @@ interface ResultMessageFormatter {
      * @param subCommand 実行されようとしたサブコマンド名。
      * @param event 解析しているイベント。
      */
-    fun onUnknownSubCommandPassed(command: String, subCommand: String, event: MessageReceivedEvent): Message
+    fun onUnknownSubCommandPassed(command: String, subCommand: String, event: MessageReceivedEvent): Sendable
 
 }
