238b4cabf9872fe0a7a49cc3bb68d63676542edb
loxygenK <loxygen.k@gmail.com>
2020-10-17 13:04:22 +0900

    †追加† デフォルトのformatterを生やした

--- src/main/kotlin/dev/approvers/jubilant/client/Client.kt
+++ src/main/kotlin/dev/approvers/jubilant/client/Client.kt
@@ -1,11 +1,16 @@
 package dev.approvers.jubilant.client
 
 import dev.approvers.jubilant.commands.CommandManager
+import dev.approvers.jubilant.commands.ResultMessageFormatter
 import dev.approvers.jubilant.commands.abc.EventListener
 import dev.approvers.jubilant.commands.abc.AbstractCommand
 import dev.approvers.jubilant.commands.event.EventInfo
 import dev.approvers.jubilant.commands.event.EventType
 import dev.approvers.jubilant.exception.TokenMissingException
+import dev.approvers.jubilant.type.sendable.EmbedSendable
+import dev.approvers.jubilant.type.sendable.Sendable
+import dev.approvers.jubilant.type.sendable.StringSendable
+import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.events.ReadyEvent
@@ -17,8 +22,50 @@ import kotlin.system.exitProcess
  * Discordのクライアント。
  */
 class Client(
-   val clientSettingInfo: ClientSettingInfo
+   val clientSettingInfo: ClientSettingInfo,
+   formatter: ResultMessageFormatter? = null
 ) : ListenerAdapter() {
+
+   private val defaultFormatter: ResultMessageFormatter = object : ResultMessageFormatter {
+      override fun onCommandSucceed(command: String, subCommand: String, event: MessageReceivedEvent): Sendable?
+              = null
+
+      override fun onCommandFailed(command: String, subCommand: String, event: MessageReceivedEvent): Sendable?
+              = null
+
+      override fun onExceptionThrown(
+         command: String,
+         subCommand: String,
+         event: MessageReceivedEvent,
+         exception: Throwable
+      ): Sendable? = EmbedSendable(
+         EmbedBuilder().apply {
+            setTitle("コマンドの実行中に例外が発生しました！")
+            setFooter("このBotのバグである可能性があります。開発者に連絡してください。")
+            setDescription("開発者向け情報:")
+            addField("例外クラス名", "`${exception::class.simpleName}`", false)
+            addField("説明", exception.message, false)
+            setColor(0xed5353)
+         }.build()
+      )
+
+      override fun onInvalidArgumentsPassed(
+         command: String,
+         subCommand: String,
+         event: MessageReceivedEvent
+      ): Sendable? = StringSendable("引数が不正です。ヘルプ(`${clientSettingInfo.prefix}$command help`)を確認してください")
+
+      override fun onUnknownCommandPassed(
+         command: String,
+         event: MessageReceivedEvent
+      ): Sendable? = StringSendable("不明なコマンドです。ヘルプ(`${clientSettingInfo.prefix}help`)を参照してください")
+
+      override fun onUnknownSubCommandPassed(
+         command: String,
+         subCommand: String,
+         event: MessageReceivedEvent
+      ): Sendable? = StringSendable("不正なサブコマンドです。ヘルプ(`${clientSettingInfo.prefix}$command help`)を確認してください")
+   }
 
    /**
     * JDA実体
@@ -28,7 +75,7 @@ class Client(
    /**
     * コマンドを管理する。
     */
-   private val commandManager: CommandManager = CommandManager(clientSettingInfo.prefix)
+   private val commandManager: CommandManager = CommandManager(clientSettingInfo.prefix, formatter ?: defaultFormatter)
 
    /**
     * コマンドを実行する
